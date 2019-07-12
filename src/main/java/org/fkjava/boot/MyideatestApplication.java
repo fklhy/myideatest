154fae1a96e9e4578dbb37e7027ed2b5bfe1c017
lhy_s <lhy_s@163.com>
2019-07-12 21:30:09 +0800

    这是一个测试

---
+++ src/main/java/org/fkjava/boot/MyideatestApplication.java
@@ -0,0 +1,13 @@
+package org.fkjava.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyideatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyideatestApplication.class, args);
+    }
+
+}
